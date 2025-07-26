3178cd6d1ed52f924ae62df358ae068c8ffe9180
Mohamed Iyed Mokline <mohamediyedmoknine2@gmail.com>
2025-07-26 05:56:50 +0100

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+# src/__init__.py
+
+"""
+Market Stress Tracker: Core package for computing and visualizing composite stress indicators.
+"""
+
+__version__ = "1.0"
+__author__ = "Mohamed Iyed Mokline"
